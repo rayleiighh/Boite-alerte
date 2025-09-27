8f065ae3b3e7d4ebc9b2533f15c7ca5609ae14ae
NicoHoed <nico.hoed@gmail.com>
2025-09-27 16:16:13 +0200

    mise en place de la structure du projet

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+/***************************************************************************
+ *
+ *  Project Title : Boite-alerte
+ *  Authors       : Nicolas H, ..., ..., ..., ...
+ *  File          : main.jsx
+ *  Description   : Entry point of the React application
+ *  Date          : 27/09/2025
+ *  Version       : [1.0.0]
+ *
+ ***************************************************************************/
 
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
