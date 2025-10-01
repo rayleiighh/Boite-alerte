73749abae8545466be82cb4af539c7ed9a366d3d
rayleiighh <r.benlhaj@students.ephec.be>
2025-10-01 11:10:16 +0200

    US – Pagination de l’historique Fixes (ajout des filtre dans l'api/events)#32

--- backend/src/routes/eventRoutes.js
+++ backend/src/routes/eventRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { addEvent } = require("../controllers/eventController");
+const { addEvent, getEvents } = require("../controllers/eventController");
 
-// POST /api/events
+// POST /api/events → ajouter un event
 router.post("/", addEvent);
+
+// GET /api/events → récupérer les events avec pagination & filtres
+router.get("/", getEvents);
 
 module.exports = router;
